dd3fd5cda3f1812aa1934c04766af671639ad146
Massa_Joe <60799887+rgunther13@users.noreply.github.com>
2020-07-25 11:57:09 -0300

    Exercicio2
    Construção

---
+++ Vendedor.java
@@ -0,0 +1,28 @@
+public class Vendedor {
+    
+    private String nome;
+    private int numeroproduto;
+    private Double valortotal;
+
+    public Vendedor(String nome, int numeroproduto, Double valortotal) {
+        this.nome = nome;
+        this.numeroproduto = numeroproduto;
+        this.valortotal = valortotal;
+    }
+    
+  
+
+    //Métodos GET
+    public String getNome(){
+        return nome;
+    }
+
+    public int getNumero(){
+        return numeroproduto;
+    }
+
+    public Double getTotal(){
+        return valortotal;
+    }
+
+}
---
+++ VendedorTeste.java
@@ -0,0 +1,25 @@
+public class VendedorTeste {
+
+    public static void main(String[] args) {
+        
+        Vendedor vendedores []  = new Vendedor [12];
+        vendedores [0] = new Vendedor("José", 1, 250.0);
+        vendedores [1] = new Vendedor("José", 2, 50.0);
+        vendedores [2] = new Vendedor("José", 3, 250.0);
+        vendedores [3] = new Vendedor("Seracapião", 1, 175.0);
+        vendedores [4] = new Vendedor("Seracapião", 2, 150.0);
+        vendedores [5] = new Vendedor("Seracapião", 3, 100.0);
+        vendedores [6] = new Vendedor("Maria", 1, 125.0);
+        vendedores [7] = new Vendedor("Maria", 2, 50.0);
+        vendedores [8] = new Vendedor("Maria", 3, 130.0);
+        vendedores [9] = new Vendedor("Eduarda", 1, 90.0);
+        vendedores [10] = new Vendedor("Eduarda", 2, 25.0);
+        vendedores [11] = new Vendedor("Eduarda", 3, 300.0);
+
+        
+
+        
+        
+    }
+    
+}
