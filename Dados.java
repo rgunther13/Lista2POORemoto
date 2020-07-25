ba09e24320246fe4b05369040a2db4a2007cf024
Massa_Joe <60799887+rgunther13@users.noreply.github.com>
2020-07-25 10:00:36 -0300

    Update Dados.java
    Mudança de nomes das funções

--- Dados.java
+++ Dados.java
@@ -1,9 +1,9 @@
 import java.util.Random;
 
 public class Dados {
-    
+
     //Função Geradora da Matriz//
-    public static void geramatiz(int matriz [] []){
+    public static void jogadados(int matriz [] []){
          //instância um objeto da classe Random usando o construtor padrão
          Random gerador = new Random();
          int linha;
@@ -12,11 +12,11 @@ public class Dados {
                 linha = (gerador.nextInt(6) +1) -1;
                 coluna = (gerador.nextInt(6) +1) -1;
                 matriz [linha] [coluna] += 1;     
-         }
+        }
     }
 
     //Função para apresentar a Matriz//
-    public static void apresentamatriz(int matriz [][]) {
+    public static void mostraresultado(int matriz [][]) {
         System.out.println("|   |\t|    1    |\t|    2    |\t|    3    |\t|    4    |\t|    5    |\t|    6    |");
         for (int i = 0; i < 6; i++){
             System.out.printf("| %d |\t", i + 1);
@@ -28,8 +28,8 @@ public class Dados {
     }
     public static void main(String[] args) {    
         int matriz [] [] = new int [6] [6];
-        geramatiz(matriz);
-        apresentamatriz(matriz);
+        jogadados(matriz);
+        mostraresultado(matriz);
 
     }
 }
