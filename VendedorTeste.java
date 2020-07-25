0d40dc1f4e9b4abd68759a4ac527d06a9aadb1e4
Massa_Joe <60799887+rgunther13@users.noreply.github.com>
2020-07-25 17:10:37 -0300

    Update VendedorTeste.java

--- VendedorTeste.java
+++ VendedorTeste.java
@@ -1,8 +1,18 @@
 public class VendedorTeste {
 
-    public static void imprimeTabela(Vendedor vendedores){
-        System.out.printf("Nome: %s Numero do Produto: %d Total: %.2f\n ", vendedores.getNome(),
-            vendedores.getNumero(), vendedores.getTotal());
+    public static void imprimeTabela(Vendedor vendedores[]){
+        
+        for (int i = 0; i < vendedores.length; i += 3 ){
+            System.out.printf("%s\t %.1f\t %.1f\t %.1f\t %.1f\t \n", vendedores[i].getNome(),
+             vendedores[i].getTotal(),
+              vendedores[i+1].getTotal(),
+               vendedores[i+2].getTotal(),
+               (vendedores[i].getTotal() + vendedores[i+1].getTotal() + vendedores[i+2].getTotal()));
+                }
+        
+        
+        //System.out.printf("Nome: %s Numero do Produto: %d Total: %.2f\n ", vendedores.getNome(),
+        //    vendedores.getNumero(), vendedores.getTotal());
     }
     public static void main(String[] args) {
         
@@ -10,9 +20,9 @@ public class VendedorTeste {
         vendedores [0] = new Vendedor("José", 1, 250.0);
         vendedores [1] = new Vendedor("José", 2, 50.0);
         vendedores [2] = new Vendedor("José", 3, 250.0);
-        vendedores [3] = new Vendedor("Seracapião", 1, 175.0);
-        vendedores [4] = new Vendedor("Seracapião", 2, 150.0);
-        vendedores [5] = new Vendedor("Seracapião", 3, 100.0);
+        vendedores [3] = new Vendedor("Jorge", 1, 175.0);
+        vendedores [4] = new Vendedor("Jorge", 2, 150.0);
+        vendedores [5] = new Vendedor("Jorge", 3, 100.0);
         vendedores [6] = new Vendedor("Maria", 1, 125.0);
         vendedores [7] = new Vendedor("Maria", 2, 50.0);
         vendedores [8] = new Vendedor("Maria", 3, 130.0);
@@ -20,8 +30,7 @@ public class VendedorTeste {
         vendedores [10] = new Vendedor("Eduarda", 2, 25.0);
         vendedores [11] = new Vendedor("Eduarda", 3, 300.0);
 
-        for(int i = 0; i < vendedores.length; i++)
-            imprimeTabela(vendedores[i]); 
+        imprimeTabela(vendedores); 
 
         
         
